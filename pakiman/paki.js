b81646653359957d4241ad3481a922c922b38ac9
Sergio Valbuena <sr.ji@SrJi-Air.gv.shawcable.net>
2021-12-20 23:24:29 -0800

    adding function to the class

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -5,11 +5,17 @@ class Pakiman
         this.nombre = n;
         this.vida = v;
         this.ataque = a;
-        this.tipo = "tierra";
+        //this.tipo = "tierra";
+    }
+    hablar()
+    {
+        alert(this.nombre);
     }
 }
 
 var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho", 80, 50);
-console.log(cauchin);
-console.log(pokacho);
+var pokacho = new Pakiman("Pokachonononnon", 80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 40);
+console.log(cauchin, pokacho, tocinauro);
+
+pokacho.hablar();
