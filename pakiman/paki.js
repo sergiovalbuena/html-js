7360345d550369175c25a75e7d699baf889cb0e6
Sergio Valbuena <sr.ji@SrJi-Air.gv.shawcable.net>
2021-12-20 23:35:40 -0800

    array added

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -1,3 +1,20 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+/*
+OBJETO LITERAL 
+JSON
+var imagenes = 
+{
+    Cauchin: "vaca.png",
+    Pokacho: "pollo.png",
+    Tocinauro: "cerdo.png"
+}
+*/ 
+
+console.log(imagenes);
 class Pakiman
 {
     constructor(n, v, a)
@@ -9,13 +26,13 @@ class Pakiman
     }
     hablar()
     {
-        alert(this.nombre);
+        //alert(this.nombre);
     }
 }
 
 var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokachonononnon", 80, 50);
+var pokacho = new Pakiman("Pokacho", 80, 50);
 var tocinauro = new Pakiman("Tocinauro", 120, 40);
 console.log(cauchin, pokacho, tocinauro);
 
-pokacho.hablar();
+//pokacho.hablar();
